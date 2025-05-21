46a93d75e33242a9135377419e02bd08f92d061d
yasmin <yg5601415@gmail.com>
2025-05-21 16:00:06 -0300

    login realizado com sucesso!

--- scripts/login.js
+++ scripts/login.js
@@ -6,21 +6,24 @@ document.addEventListener('DOMContentLoaded', function() {
     form.addEventListener('submit', function(event) {
         event.preventDefault();
 
-        const username = form.nome.value;
+        const username = form.nome.value.trim();
         const senha = form.senha.value;
-        const cadastroData = localStorage.getItem('cadastroData');
 
-        if (cadastroData) {
-            const data = JSON.parse(cadastroData);
-            if (data.nome === username && data.senha === senha) {
-                responseDiv.textContent = 'Login realizado com sucesso!';
-                // Redireciona para a página home onde está o modal
-                window.location.href = './home.html';
-            } else {
-                responseDiv.textContent = 'Nome de usuário ou senha incorretos. Por favor, verifique seus dados ou faça o cadastro.';
-            }
-        } else {
+        // Verifica se os dados do cadastro estão salvos com a chave "cadastroData"
+        const cadastroData = localStorage.getItem('cadastroData');
+        if (!cadastroData) {
             responseDiv.textContent = 'Nenhum cadastro encontrado! Por favor, cadastre-se primeiro.';
+            return;
+        }
+
+        const data = JSON.parse(cadastroData);
+        if (data.nome === username && data.senha === senha) {
+            responseDiv.textContent = 'Login realizado com sucesso!';
+            setTimeout(function() {
+                window.location.href = './home.html';
+            }, 1000);
+        } else {
+            responseDiv.textContent = 'Nome de usuário ou senha incorretos.';
         }
     });
 
